4c58f8e708cd52ba5725374d835d65b5f06b53c6
JMostafa <m.j.a.a.m94@gmail.com>
2020-07-16 16:01:57 +0200

    add SignupMarket6

--- BackEnd.API/Controllers/MarketController.cs
+++ BackEnd.API/Controllers/MarketController.cs
@@ -98,15 +98,117 @@ namespace BackEnd.API.Controllers
         }
         #endregion
 
-        #region Get: api/Market/SignupMarket2
+        #region Get: api/Market/SignupMarket4
         [HttpGet]
+        [Route("SignupMarket4")]
+        public IResponseDTO SignupMarket4(string MarketName, string MarketAddress, string MarketPhone, string MarketEmail, string MarketPassword, string MarketInfo, bool Plateform, Guid CityId, string Token, string MarketLatlng)
+        {
+            ResponseDTO res;
+            try
+            {
+                
+                var logoUrl = UploadHelper.SaveFile(Request.Form.Files[0], "logo");
+                MarketVM MarketVM = new MarketVM()
+                {
+                    CityId = CityId,
+                    MarketAddress = MarketAddress,
+                    MarketEmail = MarketEmail,
+                    MarketInfo = MarketInfo,
+                    MarketLatlng = MarketLatlng,
+                    MarketName = MarketName,
+                    MarketPassword = MarketPassword,
+                    MarketPhone = MarketPhone,
+                    Plateform = Plateform,
+                    Token = Token,
+                    MarketLogo = logoUrl,
+                };
+                return _MarketServices.PostMarket(MarketVM);
+            }
+            catch (Exception ex)
+            {
+                res = new ResponseDTO()
+                {
+                    IsPassed = false,
+                    Message = "Error in UploadMarketLog " + ex.Message,
+                    Data = null,
+                };
+            }
+            return res;
+        }
+        #endregion
+
+        #region Post: api/Market/SignupMarket5
+        [HttpPost]
+        [Route("SignupMarket5")]
+        public IResponseDTO SignupMarket5(ModelBinding.MarketImageModel model)
+        {
+            ResponseDTO res;
+            try
+            {
+                var logoUrl = UploadHelper.SaveFile(model.Image, "logo");
+                model.Market.MarketLogo = logoUrl;
+                return _MarketServices.PostMarket(model.Market);
+            }
+            catch (Exception ex)
+            {
+                res = new ResponseDTO()
+                {
+                    IsPassed = false,
+                    Message = "Error in UploadMarketLog " + ex.Message,
+                    Data = null,
+                };
+            }
+            return res;
+        }
+        #endregion
+
+        #region Post: api/Market/SignupMarket6
+        [HttpPost]
+        [Route("SignupMarket6")]
+        public IResponseDTO SignupMarket6([FromForm]MarketImageVM MarketVM)
+        {
+            ResponseDTO res;
+            try
+            {
+                var logoUrl = UploadHelper.SaveFile(MarketVM.Image, "logo");
+                MarketVM.MarketLogo = logoUrl;
+                return _MarketServices.PostMarket(new MarketVM()
+                {
+                    CityId = MarketVM.CityId,
+                    MarketAddress = MarketVM.MarketAddress,
+                    MarketEmail = MarketVM.MarketEmail,
+                    MarketInfo = MarketVM.MarketInfo,
+                    MarketLatlng = MarketVM.MarketLatlng,
+                    MarketName = MarketVM.MarketName,
+                    MarketPassword = MarketVM.MarketPassword,
+                    MarketPhone = MarketVM.MarketPhone,
+                    Plateform = MarketVM.Plateform,
+                    Token = MarketVM.Token,
+                    MarketLogo = logoUrl,
+                });
+            }
+            catch (Exception ex)
+            {
+                res = new ResponseDTO()
+                {
+                    IsPassed = false,
+                    Message = "Error in UploadMarketLog " + ex.Message,
+                    Data = null,
+                };
+            }
+            return res;
+        }
+        #endregion
+
+        #region Post: api/Market/SignupMarket2
+        [HttpPost]
         [Route("SignupMarket2")]
         public IResponseDTO SignupMarket2(string MarketName, string MarketAddress, string MarketPhone, string MarketEmail, string MarketPassword, string MarketInfo, bool Plateform, Guid CityId, string Token, string MarketLatlng)
         {
             ResponseDTO res;
             try
             {
-                
+
                 var logoUrl = UploadHelper.SaveFile(Request.Form.Files[0], "logo");
                 MarketVM MarketVM = new MarketVM()
                 {
---
+++ BackEnd.API/ModelBinding/MarketImageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.API.ModelBinding
+{
+    public class MarketImageModel
+    {
+        [Microsoft.AspNetCore.Mvc.ModelBinder(typeof(JsonWithFilesFormDataModelBinder), Name = "json")]
+        public Service.Models.MarketVM Market { get; set; }
+        public Microsoft.AspNetCore.Http.IFormFile Image { get; set; }
+
+    }
+}
